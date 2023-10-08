41379dac66850f685b3aa68ebf44d189d04cf180
Rupesh Gupta <142488982+rupesh9084@users.noreply.github.com>
2023-10-08 23:19:23 +0530

    Add files via upload

---
+++ size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+int a;
+char b;
+float c;
+double d;
+printf("the size of int:=%d\n",sizeof(a));
+printf("the size of char:=%d\n",sizeof(b));
+printf("the size of float:=%d\n",sizeof(c));
+printf("the size of double:=%d",sizeof(d));
+return 0;
+}
