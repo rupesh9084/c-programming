3fd82590c0015a40e00684fd3b9da5e70ea75e13
Rupesh Gupta <142488982+rupesh9084@users.noreply.github.com>
2023-10-10 20:48:58 +0530

    Add files via upload

---
+++ area_of_circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+int r;
+float circle;
+printf("enter the number:");
+scanf("%d",&r);
+circle=3.14*r*r;
+printf("the area of circle is:%f",circle);
+return 0;
+}
---
+++ area_of_rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+int length,bridth,area;
+printf("enter the length:");
+scanf("%d",&length);
+printf("enter the bridth:");
+scanf("%d",&bridth);
+area=length*bridth;
+printf("the area of rectangle is:%d",area);
+return 0;
+}
