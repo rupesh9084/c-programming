34bf2d4b137d360a729b26d0ef99fc955a3711af
Rupesh Gupta <142488982+rupesh9084@users.noreply.github.com>
2023-10-31 22:57:59 +0530

    Update leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -3,15 +3,8 @@ int main(){
     int year;
     printf("enter any year");
     scanf("%d",&year);
-    if(year%400==0){
-        printf("%d is a leap year",year);
-    }
-    else if(year%100==0){
-        printf("%d is a leap year",year);
-    }
-    else if(year%4==0){
-        printf("%d is a leap year ",year);
-         }
+    if(year%4==0 ||year%100!=0 ||year%400==0){
+        printf("%d is a leap year",year);}
 else
 {
     printf(" %d is a not a leap year",year);}
